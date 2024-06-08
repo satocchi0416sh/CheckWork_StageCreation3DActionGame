25ef30e964a3854677704ed788180873f4c5b69e
satocchi0416sh <83898438+satocchi0416sh@users.noreply.github.com>
2024-06-08 16:00:08 +0900

    lv3. パーツの選択・削除

--- Assets/Scripts/StageCreator.cs
+++ Assets/Scripts/StageCreator.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class StageCreator : MonoBehaviour
 {
     [SerializeField] private GameObject groundPrefab;
+    [SerializeField] private StagePartManager stagePartManager;
+    
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -13,14 +15,26 @@ public class StageCreator : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hit))
             {
-                CreateGround(hit.point);
+                if (stagePartManager.IsPartSelected) return;
+                if (hit.collider.CompareTag("Lava"))
+                {
+                    CreateGround(hit.point);
+                }
+                else
+                {
+                    SelectPart(hit.collider.gameObject);
+                }
             }
         }
-        
     }
     
     private void CreateGround(Vector3 position)
     {
         Instantiate(groundPrefab, position, Quaternion.identity);
+    }
+    
+    private void SelectPart(GameObject part)
+    {
+        stagePartManager.SelectPart(part);
     }
 }
---
+++ Assets/Scripts/StagePartManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePartManager : MonoBehaviour
+{
+    private GameObject _selectedPart;
+    private bool _isPartSelected;
+    public bool IsPartSelected => _isPartSelected;
+    
+    [SerializeField] private GameObject partButtonContainer;
+
+    private void Start()
+    {
+        _isPartSelected = false;
+        partButtonContainer.SetActive(false);
+    }
+
+    public void SelectPart(GameObject part)
+    {
+        _selectedPart = part;
+        _isPartSelected = true;
+        partButtonContainer.SetActive(true);
+    }
+
+    public void OnDeleteButtonClicked()
+    {
+        if (_isPartSelected)
+        {
+            Destroy(_selectedPart);
+            _isPartSelected = false;
+            partButtonContainer.SetActive(false);
+        }
+    }
+    
+    public void OnCancelButtonClicked()
+    {
+        _isPartSelected = false;
+        partButtonContainer.SetActive(false);
+    }
+}
